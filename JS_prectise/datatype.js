6f18624dbe1adb28f7217cf493958d146275cbab
Apple <apple@Apples-MacBook-Pro.local>
2024-03-14 14:14:50 +0530

    data types

---
+++ JS_prectise/datatype.js
@@ -0,0 +1,21 @@
+// Primitive data types
+let num = 10;           // Number
+let str = 'Hello';      // String
+let bool = true;        // Boolean
+let undef;              // Undefined
+let nul = null;         // Null
+
+// Object data type
+let obj = {             // Object
+  key: 'value',
+  age: 25
+};
+
+// Special data types
+let func = function() { // Function
+  return 'Hello World!';
+};
+
+let arr = [1, 2, 3];    // Array
+
+console.log(typeof arr);
